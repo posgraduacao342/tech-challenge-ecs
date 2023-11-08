39c2a6d5a61e6742631b21ed87f4d42a50d332b8
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2023-11-08 14:09:22 -0300

    fix: backend bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pos-graduacao-terraform-state"
+    bucket = "pos-graduacao-fiap-terraform-state"
     key    = "ecs/terraform.tfstate"
     region = "us-east-1"
   }
